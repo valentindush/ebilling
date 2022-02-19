995d8a7b5b0c42054e1d46c0a5b55d6a3d20a91d
Valentin <93974733+valentindush@users.noreply.github.com>
2022-02-19 13:37:32 +0200

    UI container

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.static())
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 
